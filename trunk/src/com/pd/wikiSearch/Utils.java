b8680497f17a4aebf11a243542d36d6dd5909dec
pasquale.damiani@gmail.com <pasquale.damiani@gmail.com@9923c822-9fb2-2c35-bdf9-8ed4550cf776>
2013-03-19 17:20:59 +0000

    removed unused constraint

--- trunk/src/com/pd/wikiSearch/Utils.java
+++ trunk/src/com/pd/wikiSearch/Utils.java
@@ -4,8 +4,6 @@ public class Utils {
 	
 	public static final String[] SERVICES = {"Wikipedia", "Wiktionary", "Wikiquote", "Wikinews", "Wikibooks"};
 	
-	public static final String TAG = "WikiSearch";
-	
 	public static final String[] WIKI = {"cus", "sq", "ar", "bg", "bs", "ca", "cs", "nl", "en", "eo", "fi", "fr", "de", "he", "hu", "it", "ja", "ko", "no", "fa", "pl", "pt", "ro", "ru", "sr", "es", "sv", "ta", "th", "tr", "uk"};
 	public static final String[] LANGUAGES = {"Custom", "Albanian", "Arabic", "Bulgarian", "Bosnian", "Catalan", "Czech", "Dutch", "English", "Esperanto", "Finnish", "French", "German", "Hebrew", "Hungarian", "Italian", "Japanese", "Korean", "Norwegian (Bokmål)", "Persian", "Polish", "Portuguese", "Romanian", "Russian", "Serbian", "Spanish", "Swedish", "Tamil", "Thai", "Turkish", "Ukrainian"};
 
